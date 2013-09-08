40a822d657badeecf5fb9267a97892ce29cb8b1c
taichi-n <taichi@nishida-no-MacBook-Pro.local>
2013-09-08 20:21:49 +0900

    first commit :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaichiTw::Application.config.secret_token = '4153e12e319769d1e7db3cc01bd6d2464520321bd1c17439521b4eaac3b095f7b13239bab3491d2f65fd3dd2b029ef63bef9e3c17618df40252551f49f2c9a86'
